c2f405cc683a0c73ef16cb4ca95938463ace88f0
dudekartik <71540594+dudekartik@users.noreply.github.com>
2021-05-07 18:26:15 +0530

    Add files via upload

---
+++ Mango.js
@@ -0,0 +1,7 @@
+class Mango extends BaseClass {
+    constructor(x,y){
+      super(x,y,30);
+      this.image = loadImage("mango.png");
+    }
+  }
+  
---
+++ chain.js
@@ -0,0 +1,38 @@
+class Shooter{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null ;
+    }
+
+    attach(bodyA){
+    this.sling.bodyA=bodyA
+    }
+
+    display(){
+            
+            if(this.sling.bodyA){
+                var pointA = this.sling.bodyA.position;
+                var pointB = this.pointB;
+            
+            strokeWeight(8);
+            stroke(48,22,8)
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            }
+                
+    
+        
+    }
+    
+}
